a8a2dd77328cdddf4dc844a0b5f26cea0992a39b
George Fairbairn <57505013+georgefairbairn@users.noreply.github.com>
2023-02-05 17:04:23 -0800

    feat: read name data from db

--- app/routes/names/$nameId.tsx
+++ app/routes/names/$nameId.tsx
@@ -5,19 +5,31 @@ import {
   RefreshCw,
   ChevronDown,
 } from "lucide-react";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  return json({
+    name: await db.name.findFirst(),
+  });
+};
 
 export default function Name() {
+  const { name } = useLoaderData<typeof loader>();
   return (
     <div className="flex justify-between max-w-4xl w-full mx-auto">
       <div className="flex flex-col justify-start w-full">
         <div className="flex justify-between w-full">
           <div className="bg-black text-white text-4xl py-5 px-10 rounded-t-lg">
-            Penelope
+            {name?.name}
           </div>
           <div className="flex items-center">
-            <div className="text-5xl text-pink-500 px-4">&#9792;</div>
+            <div className="text-5xl text-pink-500 px-4">
+              {name?.gender === "Female" ? "\u2640" : "\u2642"}
+            </div>
             <div className="text-xl italic px-4 border-x-2 border-x-black">
-              Pe‧nè‧lo‧pe
+              {name?.pronunciation}
             </div>
             <button className="px-2 mx-2 rounded-full hover:bg-black hover:bg-opacity-5">
               <Volume2 size={36} />
@@ -27,21 +39,15 @@ export default function Name() {
         <div className="flex flex-col bg-white border-4 border-black rounded-tr-lg rounded-br-lg rounded-bl-lg py-5 px-10 text-xl">
           <div className="flex w-full">
             <div className="flex-1">
-              <span className="pr-2">Origin:</span>🇬🇷
+              <span className="pr-2">Origin:</span>
+              <span className="pr-2">{name?.origin}</span>
             </div>
             <div className="flex-1">
               <span className="pr-2">Meaning:</span>
-              <span className="font-bold">"weaver"</span>
+              <span className="font-bold">"{name?.meaning.toLowerCase()}"</span>
             </div>
           </div>
-          <div className="pt-5">
-            Penelope is a name from Greek mythology; she was the wife of
-            Odysseus in Homer’s Odyssey. It has two possible origin
-            stories—Penelope was either derived from the Greek pēnē, meaning
-            "thread of a bobbin," or penelops, a type of duck. Mythological
-            Penelope was cared for by a duck as an infant, and later was known
-            for delaying her suiters by pretending...
-          </div>
+          <div className="pt-5">{name?.description}</div>
           <div className="flex justify-center mt-8">
             <button className="flex items-center text-xl px-8 py-3 rounded-full w-fit hover:bg-black hover:bg-opacity-5">
               <span className="mr-3">Read more</span>
