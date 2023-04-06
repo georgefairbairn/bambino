8cc2695fc4448c77cd4c1508b1b364bc1284cb00
George Fairbairn <57505013+georgefairbairn@users.noreply.github.com>
2023-04-06 12:29:02 -0700

    fix: return null in error case

--- app/routes/new-search/index.tsx
+++ app/routes/new-search/index.tsx
@@ -38,6 +38,7 @@ export const action = async ({ request }: ActionArgs) => {
     return redirect(`${ROUTES.SEARCH}/${search.id}`);
   } catch (error) {
     console.error(error);
+    return null;
   }
 };
 
