e4e3f7d1b6d662bd9d0aee4dd88a3bebe023421c
georgefairbairn <george.fair@icloud.com>
2024-05-13 10:34:44 +0100

    feat: add sign up

--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from '@clerk/remix';
+import { SignUp } from '@clerk/remix';
 
 export default function SignInPage() {
   return (
     <div className="flex justify-center mt-10">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
