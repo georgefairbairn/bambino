4c2739f1a63ae7c5077b4fe6f66f35a1ee307aa4
georgefairbairn <george.fair@icloud.com>
2024-05-14 17:19:40 +0100

    refactor: update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,3 @@
-import bcrypt from 'bcryptjs';
 import { PrismaClient } from '@prisma/client';
 import { boys } from './boys.json';
 import { girls } from './girls.json';
@@ -9,16 +8,9 @@ async function main() {
   try {
     // reset database
     await prisma.name.deleteMany();
-    console.log('Deleted name data');
-    await prisma.search.deleteMany();
-    console.log('Deleted searches data');
-    await prisma.user.deleteMany();
-    console.log('Deleted user data');
 
     // comment out for prod seeding
-    await prisma.$queryRaw`ALTER TABLE Name AUTO_INCREMENT = 1`;
-    await prisma.$queryRaw`ALTER TABLE Search AUTO_INCREMENT = 1`;
-    await prisma.$queryRaw`ALTER TABLE User AUTO_INCREMENT = 1`;
+    await prisma.$queryRaw`ALTER SEQUENCE "Name_id_seq" RESTART WITH 1;`;
     console.log('Reset auto-increments');
 
     // add name data
@@ -27,21 +19,6 @@ async function main() {
       skipDuplicates: true,
     });
     console.log('Seeded name data');
-
-    const passwordHash = await bcrypt.hash(
-      process.env.ADMIN_PASSWORD ?? 'password',
-      10
-    );
-
-    // add single user
-    await prisma.user.create({
-      data: {
-        name: 'George Admin',
-        email: 'george.fair@icloud.com',
-        passwordHash,
-      },
-    });
-    console.log('Seeded user data');
   } catch (error) {
     console.error(error);
   } finally {
