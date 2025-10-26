754cdf8e7c615de5fa922ccf601878fafea110f0
George Fairbairn <your_personal_email@example.com>
2025-10-26 09:23:11 +0000

    feat: add prettier babel and tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const pluginPrettier = require('eslint-plugin-prettier');
+const configPrettier = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   expoConfig,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      'prettier/prettier': ['error'],
+    },
+  },
+  configPrettier,
   {
     ignores: ['dist/*'],
   },
